3ae78e46a16df87da1f462fc1dbab75f9ec24bd1
Fakunle Israel <maxibrain4@gmail.com>
2023-04-10 17:12:06 +0100

    added some event handler to the js

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 "use strict";
-const btnSubmit = document.querySelector(".btn");
-const errorMessageOne = document.querySelector(".errorMessage1");
-const errorMessageTwo = document.querySelector(".errorMessage2");
-let x = document.querySelector(".largeForm2").value;
+// const btnSubmit = document.querySelector(".btn");
+// const errorMessageOne = document.querySelector(".errorMessage1");
+// const errorMessageTwo = document.querySelector(".errorMessage2");
+// let x = document.querySelector(".largeForm2").value;
 
-btnSubmit.addEventListener("click", function () {
-  errorMessageOne.innerHTML = "";
-  try {
-    if (x.trim() == "") throw "input can not be empty";
-    if (isNaN(x)) throw "Wrong format, numbers only";
-  } catch (err) {
-    errorMessageOne.innerHTML = err;
-  }
+// btnSubmit.addEventListener("click", function () {
+//   errorMessageOne.innerHTML = "";
+//   try {
+//     if (x.trim() == "") throw "input can not be empty";
+//     if (isNaN(x)) throw "Wrong format, numbers only";
+//   } catch (err) {
+//     errorMessageOne.innerHTML = err;
+//   }
+// });
+const nameInput = document.querySelector(".largeForm");
+const cardName = document.querySelector(".cardName");
+const numInput = document.querySelector(".largeForm2");
+const cardNumber = document.querySelector(".card-number");
+const ccvNumber = document.querySelector(".ccv");
+const ccvInput = document.querySelector(".ccvinput");
+nameInput.addEventListener("input", function () {
+  cardName.textContent = nameInput.value;
+});
+
+numInput.addEventListener("input", function () {
+  cardNumber.textContent = numInput.value;
+});
+
+ccvInput.addEventListener("input", function () {
+  ccvNumber.textContent = ccvInput.value;
 });
